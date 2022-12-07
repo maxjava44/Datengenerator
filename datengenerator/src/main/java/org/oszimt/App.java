49bcc18705878977ca8a91b2373529340edd8114
maximumdeveloping <max.gilsoul@live.com>
2022-12-07 17:48:19 +0100

    Update App.java

--- datengenerator/src/main/java/org/oszimt/App.java
+++ datengenerator/src/main/java/org/oszimt/App.java
@@ -17,18 +17,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        List<Region> regions = new ArrayList<>();
-        File[] regionFiles = Paths.get(".","regionen").toFile().listFiles(pathname -> !pathname.toString().contains(".DS_Store"));
-        if(regionFiles != null){
-            Arrays.stream(regionFiles).forEach(file -> regions.add(new Region(file.toPath().toAbsolutePath().normalize().toString())));
-        }
-        for(Region region : regions) {
-            for(Land land : region.getLaender()){
-                if(land.getLandName().equals("Argentinien")){
-                    Arrays.stream(GenerateNames.generateNames(land, 30,true)).forEach(System.out::println);
-                }
-            }
-        }
 
         new MainWindow();
 
